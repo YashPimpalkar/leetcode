e635f1e74adf3b4953b61a47b69135a85b88cf8f
yashpimpalkar <yashpimpalkar214@gmail.com>
2025-10-06 14:25:13 +0530

    Add implementation and documentation for createHelloWorld function

---
+++ 2667. Create Hello World Function/hello.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+const f = createHelloWorld();
+console.log(f()); // Output: "Hello World"
+console.log(f({}, null, 42)); // Output: "Hello World" (arguments are ignored)
